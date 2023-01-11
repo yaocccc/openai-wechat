498dcab0e70c77d6a08b3b248b96d71cc3b508f9
chenyc <flyforest@outlook.com>
2023-01-11 18:14:00 +0800

    add webapi support

--- src/openai.mjs
+++ src/openai.mjs
@@ -1,5 +1,6 @@
 import {Configuration, OpenAIApi} from "@fortaine/openai";
 import {streamCompletion} from "@fortaine/openai/stream";
+import axios from "axios";
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -7,6 +8,10 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const send_to_openai = async (prompt) => {
+    if (process.env.OPENAI_WEB_API != "") {
+        const res = await axios.post(process.env.OPENAI_WEB_API, {q: prompt});
+        return res.data;
+    }
     try {
         const max_tokens = 2048 - prompt.length;
 
@@ -55,4 +60,4 @@ const send_to_openai = async (prompt) => {
     }
 }
 
-export { send_to_openai };
+export {send_to_openai};
